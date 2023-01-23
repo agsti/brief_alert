f23fcdb13f9ec4b74bd623390ef9fd8829e8b5a5
Agusti Bau <agustibau@gmail.com>
2023-01-23 17:08:09 -0300

    Added robots.txt

--- landing_page/site/gatsby-config.js
+++ landing_page/site/gatsby-config.js
@@ -29,6 +29,14 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'content/assets/brief_alert/favicon.png'
       }
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://www.briefalert.io',
+        sitemap: 'https://www.briefalert.io/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }]
+      }
     }
   ],
   // Customize your site metadata
